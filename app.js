ebe4a53b9a8ffc448a13f7c16174cba5eb796c71
Henry Niermann <henryniermann4@gmail.com>
2020-06-17 19:14:20 -0400

    basic working!

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io') (http);
 var path = require('path');
-var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://hjn80:painandsuffer@cluster0-sxq4m.azure.mongodb.net/videos?retryWrites=true&w=majority';
-mongoose.connect(mongoDB, { useNewUrlParser: true });
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// var mongoose = require('mongoose');
+// var mongoDB = 'mongodb+srv://hjn80:painandsuffer@cluster0-sxq4m.azure.mongodb.net/videos?retryWrites=true&w=majority';
+// mongoose.connect(mongoDB, { useNewUrlParser: true });
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 var Conversation = require('../messenger/models/convo');
 
@@ -16,9 +16,7 @@ app.get('/',function(req,res) {
 });
 
 io.on('connection', (socket) => {
-    console.log('Connection logged');
     socket.on('chat message', (msg) => {
-        console.log(msg);
         io.emit('chat message', msg);
     });
 });
